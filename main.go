235f3248190669ff014b2a4e281effdbd36d3e06
candraagustianp <candraagustianp@gmail.com>
2021-12-25 12:56:54 +0700

    inisialisasi awal

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.SendString("hello world")
+	})
+
+	app.Listen(":3000")
+}
